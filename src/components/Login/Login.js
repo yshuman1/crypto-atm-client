6e4f33ad6cb4cb340e4fb7eddacdd4c171d9b0ca
Yasin Shuman <email@yasin.io>
2018-06-08 15:16:18 -0700

    added some error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,6 +14,13 @@ class Login extends Component {
     };
   }
 
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, info);
+  }
+
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
