d100a457d39818095faa8cb9aa1c7490cbaba9bc
Yasin Shuman <email@yasin.io>
2018-06-09 15:02:50 -0700

    trying to wrap apikey

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyAlEcysmtEdU9xV3NFoqRD9jF7BFty1Fbo",
+  apiKey: `${process.env.API_KEY}`,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DB_URL,
   projectId: process.env.PROJ_ID,
