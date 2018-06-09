796e7de08293e4b83a79cf9802c419c1ed40e396
Yasin Shuman <email@yasin.io>
2018-06-09 16:51:24 -0700

    forgot gitignore

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.use(cors());
 server.use(express.json());
 server.use(express.static(path.join(__dirname, "client/build")));
 server.get("/", (req, res) => {
-  res.send(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 server.listen(process.env.PORT || 3001, () => {
   const port = process.env.PORT ? process.env.PORT : 3001;
