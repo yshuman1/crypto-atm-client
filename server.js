0008b1625887fa90427f22348bb8f6278d3192a3
Yasin Shuman <email@yasin.io>
2018-06-09 16:45:28 -0700

    testing endpoint

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(express.static(path.join(__dirname, "client/build")));
-
+server.get("/", () => {
+  res.json({ message: "works fine" });
+});
 server.listen(process.env.PORT || 3001, () => {
   const port = process.env.PORT ? process.env.PORT : 3001;
   console.log(`server running on port ${port}`);
