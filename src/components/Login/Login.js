6ad9821ec47894db7c6aa64e9ee9fc522114b959
Yasin Shuman <email@yasin.io>
2018-06-09 01:03:25 -0700

    changed syntax to match jsx

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -49,35 +49,39 @@ class Login extends Component {
     return (
       <div className="col-md-6">
         <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+          <div className="form-group">
+            <label htmlfor="exampleInputEmail1">Email address</label>
             <input
               value={this.state.email}
               onChange={this.handleChange}
               type="email"
               name="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
             />
-            <small id="emailHelp" class="form-text text-muted">
+            <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
             </small>
           </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
+          <div className="form-group">
+            <label htmlfor="exampleInputPassword1">Password</label>
             <input
               value={this.state.password}
               onChange={this.handleChange}
               type="password"
               name="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
             />
           </div>
-          <button type="submit" onClick={this.login} class="btn btn-primary">
+          <button
+            type="submit"
+            onClick={this.login}
+            className="btn btn-primary"
+          >
             Login
           </button>
           <button
