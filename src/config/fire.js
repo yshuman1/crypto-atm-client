3a9bd9f4d8f3651ebad041ab294f0857e22c82e9
Yasin Shuman <email@yasin.io>
2018-06-09 15:23:08 -0700

    why is my key not able to be held in env

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,8 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-const apiKeyFirebase = process.env.API_KEY;
 const config = {
-  apiKey: `${apiKeyFirebase}`,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DB_URL,
   projectId: process.env.PROJ_ID,
