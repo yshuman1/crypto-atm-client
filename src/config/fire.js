91db17c4ded7de4a00d0e04ef14d72d2dff3e566
Yasin Shuman <email@yasin.io>
2018-06-09 15:16:02 -0700

    give me my key dorks

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,8 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-
+const apiKeyFirebase = process.env.API_KEY;
 const config = {
-  apiKey: `${process.env.API_KEY}`,
+  apiKey: `${apiKeyFirebase}`,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DB_URL,
   projectId: process.env.PROJ_ID,
